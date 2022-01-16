c7ff808b8942327f1914193c6b18ea52d8bd062e
hariskurtishi <Hariskay95@gmail.com>
2022-01-16 20:09:59 +0100

    Hulu 2.0

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = react.forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
